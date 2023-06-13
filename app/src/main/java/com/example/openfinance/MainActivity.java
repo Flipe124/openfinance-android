e58e05ace1a296199e0463e3aa48c6fc0d33159e
Felipe <flipeoliveira007@gmail.com>
2023-06-13 20:13:29 -0300

    add

--- app/src/main/java/com/example/openfinance/MainActivity.java
+++ app/src/main/java/com/example/openfinance/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.openfinance;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -76,3 +80,26 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 }
+
+//    public class MeuFragmento extends Fragment {
+//
+//        private Button meuBotao;
+//        private NavController navController;
+
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//            View view = inflater.inflate(R.layout.fragment_slideshow, container, false);
+//
+//            meuBotao = view.findViewById(R.id.botao_nova_despesa);
+//            navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+//
+//            meuBotao.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    navController.navigate(R.id.nav_home);
+//                }
+//            });
+//
+//            return view;
+//        }
+//    }
